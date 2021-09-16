6a4db2576fe60fa67a4aa1f2b5c8678917c732c6
Jakob Braun <jakob.braun@posteo.de>
2021-09-16 10:19:37 -0300

    feature/4: Add getting different projects versions

--- src/test/java/com/exasol/mavenprojectversiongetter/MavenProjectVersionGetterTest.java
+++ src/test/java/com/exasol/mavenprojectversiongetter/MavenProjectVersionGetterTest.java
@@ -3,11 +3,25 @@ package com.exasol.mavenprojectversiongetter;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class MavenProjectVersionGetterTest {
+
     @Test
-    void test() {
-        assertThat(MavenProjectVersionGetter.getCurrentProjectVersion(), equalTo("0.1.0"));
+    void testReadVersion(@TempDir final Path tempDir) throws IOException {
+        final Path pomFile = tempDir.resolve("pom.xml");
+        Files.writeString(pomFile, "<project><version>1.2.3</version></project>");
+        assertThat(MavenProjectVersionGetter.getProjectVersion(pomFile), equalTo("1.2.3"));
+    }
+
+    @Test
+    void testGetVersionOfCurrentProject() {
+        assertThat(MavenProjectVersionGetter.getCurrentProjectVersion(),
+                equalTo(MavenProjectVersionGetter.getProjectVersion(Path.of("pom.xml"))));
     }
 }
